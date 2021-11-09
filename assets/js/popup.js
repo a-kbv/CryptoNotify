72feb437a904b13121535374c3bb55143f155d93
a-kbv <atanaskorabov@gmail.com>
2021-11-10 00:58:55 +0200

    k

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -30,30 +30,114 @@ notify.addEventListener("click", () => {
 });
 
 notify2.addEventListener("click", () => {
-  var promise = $.ajax({
-    method: "GET",
-    url: "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=squid-game",
-  });
+//   var promise = $.ajax({
+//     method: "GET",
+//     url: "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?slug=squid-game",
+//   });
 
-  promise.then(
-    function (response) {
-      console.log(response);
-      var pesho =
-        response.data.name +
-        " " +
-        response.data.marketPairs[1].price +
-        " " +
-        response.data.marketPairs[1].quoteSymbol +
-        " " +
-        response.data.marketPairs[1].lastUpdated;
-      $(".price").text(pesho);
+//   promise.then(
+//     function (response) {
+//       console.log(response);
+//       var pesho =
+//         response.data.name +
+//         " " +
+//         response.data.marketPairs[1].price +
+//         " " +
+//         response.data.marketPairs[1].quoteSymbol +
+//         " " +
+//         response.data.marketPairs[1].lastUpdated;
+//       $(".price").text(pesho);
 
-      chrome.runtime.sendMessage("", {
-        type: "notification",
-        message: pesho,
-        icon: "assets/images/icon-128.png",
-      });
-    },
-    function (reason) {}
-  );
+//       chrome.runtime.sendMessage("", {
+//         type: "notification",
+//         message: pesho,
+//         icon: "assets/images/icon-128.png",
+//       });
+//     },
+//     function (reason) {}
+var ws = new WebSocket("wss://stream.coinmarketcap.com/price/latest"),
+    l = {
+      timeout: 3e5,
+      timeoutObj: null,
+      serverTimeoutObj: null,
+      reset: function () {
+        clearTimeout(this.timeoutObj),
+          clearTimeout(this.serverTimeoutObj),
+          this.start();
+      },
+      start: function () {
+        var e = this;
+        this.timeoutObj = setTimeout(function () {
+          ws.send("ping"),
+            (e.serverTimeoutObj = setTimeout(function () {
+              ws.close();
+            }, e.timeout));
+        }, this.timeout);
+      },
+    };
+  ws.onopen = function () {
+    l.start();
+    $(".notify-display").text("Connection open ...");
+    var cryptoData = {
+      method: "subscribe",
+      id: "price",
+      data: {
+        cryptoIds: [13276,1],
+        index: "detail",
+      },
+    };
+    ws.send(JSON.stringify(cryptoData)), (p = 15e3), clearTimeout(1);
+  };
+  ws.onmessage = function (e) {
+    var r = JSON.parse(e.data);
+    console.log(r.d);
+    $(".price").text(r.d.cr.p);
+    $(".notify-display").text("Last update date: " + Date((r.d.t + 7200) * 1000));
+  };
+  ws.onerror = function (e) {
+    console.error("Socket encountered error: ", e.message, "Closing socket"),
+      ws.close();
+  };
 });
+
+//   var ws = new WebSocket("wss://stream.coinmarketcap.com/price/latest"),
+//     l = {
+//       timeout: 3e5,
+//       timeoutObj: null,
+//       serverTimeoutObj: null,
+//       reset: function () {
+//         clearTimeout(this.timeoutObj),
+//           clearTimeout(this.serverTimeoutObj),
+//       },
+//       start: function () {
+//         var e = this;
+//         this.timeoutObj = setTimeout(function () {
+//           ws.send("ping"),
+//             (e.serverTimeoutObj = setTimeout(function () {
+//               ws.close();
+//             }, e.timeout));
+//         }, this.timeout);
+//       },
+//     };
+//   ws.onopen = function () {
+//     l.start();
+//     $(".notify-display").text("Connection open ...");
+//     var cryptoData = {
+//       method: "subscribe",
+//       id: "price",
+//       data: {
+//         cryptoIds: [13276],
+//         index: "detail",
+//     };
+//     ws.send(JSON.stringify(cryptoData)), (p = 15e3), clearTimeout(1);
+//   };
+//   ws.onmessage = function (e) {
+//     var r = JSON.parse(e.data);
+//     console.log(r.d);
+//     $(".price").text(r.d.cr.p);
+//     $(".display").text("Last update date: " + Date((r.d.t + 7200) * 1000));
+//   };
+//   ws.onerror = function (e) {
+//     console.error("Socket encountered error: ", e.message, "Closing socket"),
+//       ws.close();
+//   };
--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+import "assets/js/jquery-3.6.0.js";
+
 chrome.runtime.onMessage.addListener( data => {
 	if ( data.type === 'notification' ) {
 		notify( data.message );
@@ -34,3 +36,64 @@ const notify = message => {
 		}
 	);
 };
+
+// function createWebSocketConnection() {
+//     if('WebSocket' in window){
+//         chrome.storage.local.get("instance", function(data) {
+//             connect('wss://' + data.instance + 'stream.coinmarketcap.com/price/latest');
+//         });
+//     }
+// }
+
+// //Make a websocket connection with the server.
+// function connect(host) {
+//     if (websocket === undefined) {
+//         websocket = new WebSocket("stream.coinmarketcap.com/price/latest");
+//     }
+// }
+//https://stackoverflow.com/questions/48746426/implement-websocket-in-chrome-extension
+
+
+var ws = new WebSocket("wss://stream.coinmarketcap.com/price/latest");
+    l = {
+      timeout: 3e5,
+      timeoutObj: null,
+      serverTimeoutObj: null,
+      reset: function () {
+        clearTimeout(this.timeoutObj),
+          clearTimeout(this.serverTimeoutObj),
+          this.start();
+      },
+      start: function () {
+        var e = this;
+        this.timeoutObj = setTimeout(function () {
+          ws.send("ping"),
+            (e.serverTimeoutObj = setTimeout(function () {
+              ws.close();
+            }, e.timeout));
+        }, this.timeout);
+      },
+    };
+  ws.onopen = function () {
+    l.start();
+    $(".notify-display").text("Connection open ...");
+    var cryptoData = {
+      method: "subscribe",
+      id: "price",
+      data: {
+        cryptoIds: [13276,1],
+        index: "detail",
+      },
+    };
+    ws.send(JSON.stringify(cryptoData)), (p = 15e3), clearTimeout(1);
+  };
+  ws.onmessage = function (e) {
+    var r = JSON.parse(e.data);
+    console.log(r.d);
+    $(".price").text(r.d.cr.p);
+    $(".notify-display").text("Last update date: " + Date((r.d.t + 7200) * 1000));
+  };
+  ws.onerror = function (e) {
+    console.error("Socket encountered error: ", e.message, "Closing socket"),
+      ws.close();
+  };
